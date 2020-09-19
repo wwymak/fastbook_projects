332be459d16c3862eda361b309772f6d28e6c7c3
wwymak <wwymak@gmail.com>
2020-09-19 22:03:00 +0100

    first streamlit app

---
+++ apps/instrument_classifier/instrument_classifier.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from fastai.learner import load_learner
+from fastai.vision.core import PILImage
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.title('Instrument classifier')
+st.markdown(f'currently supported instruments: `acoustic_guitar`,'
+            f'`bass_guitar`, `drums`, `flute`, `gramophone`, `harp`, `piano`,'
+            f' `saxophone`, `tabla`, `violen`')
+uploader_image = st.file_uploader("upload your image to be classified", key="input_image_loader")
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return load_learner("instrument_classifier.pkl")
+
+
+instrument_model = load_model()
+
+if uploader_image is not None:
+    image = PILImage.create(uploader_image)
+
+    prediction, prediction_index, probability = instrument_model.predict(image)
+    st.text(f"predicted: {prediction}, probability: {probability[prediction_index]}")
+    st.image(image)
